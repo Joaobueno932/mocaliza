e9072c02114e0e925db6b06cfb3a612565285855
Joaobueno932 <joaobuenopinheiro@gmail.com>
2023-10-26 20:45:43 -0400

    aula 26-10-23

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const {Database} = require('./configs/sequelize');
+const{Category} = require('./models/category');
 
 const db = Database.getInstance();
 
 const app = express();
 
+app.get('/api/categories', async (req,res) => {
+	const categories = Category.findAll();
+
+	res.json({
+		data: categories
+	});
+});
+
+app.post('/api/categories', async (req,res) => {
+	const categories = await Category.create(req.params);
+	res.json({
+		data: categories
+	});
+});
+
 app.listen(8000, async() => {
-	require('./models/category');
 	
 	await db.sync();
 	console.log('Server is running on port 8000!');
