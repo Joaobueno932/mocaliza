a089d4ea2059de92806e89f36b67dd14ae888283
Joaobueno932 <joaobuenopinheiro@gmail.com>
2023-10-26 20:30:39 -0400

    Aula 26-10-23

---
+++ src/main.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const {Database} = require('./configs/sequelize');
+
+const db = Database.getInstance();
+
+const app = express();
+
+app.listen(8000, async() => {
+	require('./models/category');
+	
+	await db.sync();
+	console.log('Server is running on port 8000!');
+});
